176af15d61dc9ead1e7216d20cc5344ed0c90088
rl58xz <renliang1000@163.com>
2020-09-28 22:48:01 +0800

    完成元素更新

--- toy-react.js
+++ toy-react.js
@@ -6,7 +6,13 @@ class ElementWrapper{
     }
 
     setAttribute(name,value){
-        this.root.setAttribute(name,value)
+        if(name.match(/^on([\s\S]+)$/)){
+            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLocaleLowerCase()),value);
+        }else{
+            if(name == 'className'){
+                this.root.setAttribute('class',value);
+            }else this.root.setAttribute(name,value);
+        } 
     }
 
     appendChild(component){
@@ -36,6 +42,7 @@ export class Component{
         this.props = Object.create(null);
         this.children = [];
         this._root = null;
+        this._range = null;
     }
     
     setAttribute(name,value){
@@ -47,7 +54,38 @@ export class Component{
     }
 
     [RENDER_TO_DOM](range){
+        this._range = range;
         this.render()[RENDER_TO_DOM](range);
+    }
+
+    rerender(){
+        let oldRange = this._range;
+        let range = document.createRange();
+        range.setStart(oldRange.startContainer,oldRange.startOffset);
+        range.setEnd(oldRange.startContainer,oldRange.startOffset);
+        this[RENDER_TO_DOM](range);
+
+        oldRange.setStart(range.endContainer, range.endOffset);
+        oldRange.deleteContents();
+    }
+
+    setState(newState){
+        if(this.state === null || typeof this.state !== 'object'){
+            this.state = newState;
+            this.rerender();
+            return;
+        }
+        let merge = (oldState,newState) => {
+            for(let p in newState){
+                if(oldState[p] === null || typeof oldState[p] !== 'object'){
+                    oldState[p] = newState[p];
+                }else{
+                    merge(oldState[p],newState[p]);
+                }
+            }
+        }
+        merge(this.state,newState);
+        this.rerender();
     }
 }
 
@@ -68,7 +106,8 @@ export function createElement(type, attributes, ...children){
             if(typeof child === 'string'){
                 child = new Text(child)
             }
-    
+
+            if(child === null) continue;
             if(typeof child === 'object' && child instanceof Array){
                 insertChild(child);
             }else e.appendChild(child);
